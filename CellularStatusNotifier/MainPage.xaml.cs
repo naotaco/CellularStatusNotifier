8529e4af832f5ad612e074a1aae6b0a3a46c85f5
Nao Taco <naotaco@gmail.com>
2018-12-24 16:47:37 +0900

    init

---
+++ CellularStatusNotifier/MainPage.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking.Connectivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.UI.Notifications;
+using System.Text;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CellularStatusNotifier
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Debug.WriteLine("Hello");
+
+            NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+
+            var profiles = NetworkInformation.GetConnectionProfiles();
+            PrintStatus(profiles);
+
+            var p = NetworkInformation.GetInternetConnectionProfile();
+            NotifyCurrentStatus(p);
+        }
+
+        private static void NotifyCurrentStatus(ConnectionProfile p)
+        {
+            if (p.IsWwanConnectionProfile)
+            {
+                Debug.WriteLine("Current: WWan");
+                var sb = new StringBuilder();
+                sb.AppendLine("WWan: " + p.GetNetworkNames().Aggregate((a, b) => a + " " + b));
+
+                if (p.GetSignalBars() != null)
+                {
+                    sb.AppendLine("Level: " + p.GetSignalBars().Value);
+                }
+
+                var detail = p.WwanConnectionProfileDetails;
+                if (detail != null)
+                {
+                    sb.AppendLine("APN: " + detail.AccessPointName);
+                    sb.AppendLine("IPKind: " + detail.IPKind);
+                    sb.AppendLine("Registration state: " + detail.GetNetworkRegistrationState().ToString());
+                    sb.AppendLine("Connection type: " + detail.GetCurrentDataClass().ToString());
+                }
+
+                InvokeNotification(sb.ToString());
+            }
+            else if (p.IsWlanConnectionProfile)
+            {
+                Debug.WriteLine("Current: WLan");
+                var sb = new StringBuilder();
+                sb.Append("WLan: " + p.ProfileName);
+                sb.Append(" ");
+                sb.Append(p.GetNetworkNames().Aggregate((a, b) => a + " " + b));
+                sb.Append(" Level: ");
+                if (p.GetSignalBars() != null)
+                {
+                    sb.Append(p.GetSignalBars().Value);
+                }
+                InvokeNotification(sb.ToString());
+            }
+            else
+            {
+                Debug.WriteLine("No service.");
+                InvokeNotification("No service.");
+            }
+        }
+
+        static void InvokeNotification(string text)
+        {
+            ToastVisual visual = new ToastVisual()
+            {
+                BindingGeneric = new ToastBindingGeneric()
+                {
+                    Children =
+                    {
+            new AdaptiveText()            {                Text = text            }
+                    }
+                }
+            };
+
+            ToastContent toastContent = new ToastContent()
+            {
+                Visual = visual,
+            };
+
+            // And create the toast notification
+            var toast = new ToastNotification(toastContent.GetXml());
+            toast.ExpirationTime = DateTime.Now.AddSeconds(5);
+
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+        private void NetworkInformation_NetworkStatusChanged(object sender)
+        {
+            var profiles = NetworkInformation.GetConnectionProfiles();
+            PrintStatus(profiles);
+            var p = NetworkInformation.GetInternetConnectionProfile();
+            if (p != null)
+            {
+                NotifyCurrentStatus(p);
+            }
+        }
+
+        private static void PrintStatus(IReadOnlyList<ConnectionProfile> profiles)
+        {
+            return;
+
+            if (profiles != null)
+            {
+                Debug.WriteLine("ok");
+            }
+            else
+            {
+                return;
+            }
+
+
+
+            foreach (var p in profiles)
+            {
+
+                if (p.IsWlanConnectionProfile)
+                {
+                    Debug.WriteLine("Wi-Fi ");
+                    foreach (var n in p.GetNetworkNames())
+                    {
+                        Debug.WriteLine(n);
+                    }
+                }
+                else if (p.IsWwanConnectionProfile)
+                {
+                    Debug.WriteLine("WWan");
+                }
+                foreach (var n in p.GetNetworkNames())
+                {
+                    Debug.WriteLine(n);
+                }
+            }
+        }
+    }
+}

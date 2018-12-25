5bf7627ff0d20332bedfba74736f09449bfcc470
Nao Taco <naotaco@gmail.com>
2018-12-25 21:52:11 +0900

    add worker

---
+++ CellularStatusNotifier/NetworkStatusObserver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+
+namespace CellularStatusNotifier
+{
+    class NetworkStatusObserver : IBackgroundTask
+    {
+        private BackgroundTaskDeferral backgroundTaskDeferral;
+        private AppServiceConnection appServiceconnection;
+        private String[] inventoryItems = new string[] { "Robot vacuum", "Chair" };
+        private double[] inventoryPrices = new double[] { 129.99, 88.99 };
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            this.backgroundTaskDeferral = taskInstance.GetDeferral(); // Get a deferral so that the service isn't terminated.
+            taskInstance.Canceled += OnTaskCanceled; // Associate a cancellation handler with the background task.
+
+            // Retrieve the app service connection and set up a listener for incoming app service requests.
+            var details = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            appServiceconnection = details.AppServiceConnection;
+            appServiceconnection.RequestReceived += OnRequestReceived;
+            Debug.WriteLine("run");
+        }
+
+        private async void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            // This function is called when the app service receives a request
+            Debug.WriteLine("Request Received");
+        }
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            if (this.backgroundTaskDeferral != null)
+            {
+                // Complete the service deferral.
+                this.backgroundTaskDeferral.Complete();
+            }
+
+            Debug.WriteLine("Task canceled");
+        }
+    }
+}

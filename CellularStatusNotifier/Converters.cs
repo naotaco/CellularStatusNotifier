398e211c0fd27a0aed95ddcab528ac4087592218
Nao Taco <naotaco@gmail.com>
2018-12-26 16:01:36 +0900

    show only wlan or wwan depends on current status

--- CellularStatusNotifier/Converters.cs
+++ CellularStatusNotifier/Converters.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Networking.Connectivity;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 
 namespace CellularStatusNotifier
@@ -26,4 +27,125 @@ namespace CellularStatusNotifier
             throw new NotImplementedException();
         }
     }
+
+    public class SignalLevelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) { return "-"; }
+
+            var p = value as ConnectionProfile;
+            var l = p.GetSignalBars();
+            if (l == null) { return "-"; }
+            return l.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WlanVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) { return Visibility.Collapsed; }
+
+            var p = value as ConnectionProfile;
+            if (p.IsWlanConnectionProfile) { return Visibility.Visible; }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WwanVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) { return Visibility.Collapsed; }
+
+            var p = value as ConnectionProfile;
+            if (p.IsWwanConnectionProfile) { return Visibility.Visible; }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WwanConnectionTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) { return "-"; }
+
+            var p = value as ConnectionProfile;
+            if (p.WwanConnectionProfileDetails == null) { return null; }
+            switch (p.WwanConnectionProfileDetails.GetCurrentDataClass())
+            {
+                case WwanDataClass.Cdma1xEvdo:
+                    return "CDMA 1x EVDO";
+                case WwanDataClass.Cdma1xEvdoRevA:
+                    return "CDMA 1x EVDO Rev. A";
+                case WwanDataClass.Cdma1xEvdoRevB:
+                    return "CDMA 1x EVDO Rev. B";
+                case WwanDataClass.Cdma1xEvdv:
+                    return "CDMA 1x EVDV";
+                case WwanDataClass.Cdma1xRtt:
+                    return "CDMA 1x RTT";
+                case WwanDataClass.Cdma3xRtt:
+                    return "CDMA 3x RTT";
+                case WwanDataClass.CdmaUmb:
+                    return "CDMA UMB";
+                case WwanDataClass.Custom:
+                    return "Other";
+                case WwanDataClass.Edge:
+                    return "EDGE";
+                case WwanDataClass.Gprs:
+                    return "GPRS";
+                case WwanDataClass.Hsdpa:
+                    return "HSDPA";
+                case WwanDataClass.Hsupa:
+                    return "HSUPA";
+                case WwanDataClass.LteAdvanced:
+                    return "LTE Advanced";
+                case WwanDataClass.None:
+                    return "None";
+                case WwanDataClass.Umts:
+                    return "UMTS";
+                default:
+                    return "Unknown";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SsidConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) { return "-"; }
+
+            var p = value as ConnectionProfile;
+            if (p.WlanConnectionProfileDetails == null) { return "-"; }
+            return p.WlanConnectionProfileDetails.GetConnectedSsid();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
